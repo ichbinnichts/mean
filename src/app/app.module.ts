7e266da5d39b1bd42d7373722de8bc2a0236b47e
Nathan Víctor de Faria <nathan.faria@inatel.br>
2023-01-19 14:44:48 -0300

    feat: add SchoolComponent to AppModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import {SchoolComponent} from "./school/school.component"
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -5,7 +6,8 @@ import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SchoolComponent
   ],
   imports: [
     BrowserModule
