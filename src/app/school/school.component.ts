f7e0f0a585240427c650de8638bdecf1f881a1c2
Nathan Víctor de Faria <nathan.faria@inatel.br>
2023-01-20 10:43:17 -0300

    property binding

--- src/app/school/school.component.ts
+++ src/app/school/school.component.ts
@@ -1,4 +1,4 @@
-import {Component } from "@angular/core";
+import {Component, OnInit } from "@angular/core";
 
 @Component({
     selector: 'app-school',
@@ -6,6 +6,15 @@ import {Component } from "@angular/core";
 })
 
 export class SchoolComponent {
+
+    constructor () {}
+
+    ngOnInit(): void {
+        setTimeout(() => {
+            this.acceptNewSchool = true;
+        }, 3000);
+    }
     schoolId: number = 10;
     schoolName: string = "Highriders school";
+    acceptNewSchool: boolean = false;
 }
