786aca39f5997617a5402aaf321f2dc5f84ad9b7
Nathan Víctor de Faria <nathan.faria@inatel.br>
2023-01-23 17:29:29 -0300

    correct some mistakes left behind

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,12 @@
-import {SchoolComponent} from "./school/school.component"
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { SchoolsComponent } from './schools/schools.component';
 import { FormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent,
-    SchoolComponent,
-    SchoolsComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
