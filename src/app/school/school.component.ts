433c5ea210976ae31e3bc9d68fe928895bbdb609
Nathan Víctor de Faria <nathan.faria@inatel.br>
2023-01-23 11:42:49 -0300

    two way databinding

--- src/app/school/school.component.ts
+++ src/app/school/school.component.ts
@@ -2,7 +2,8 @@ import {Component, OnInit } from "@angular/core";
 
 @Component({
     selector: 'app-school',
-    templateUrl: './school.component.html'
+    templateUrl: './school.component.html',
+    styleUrls: ['./school.component.css']
 })
 
 export class SchoolComponent {
