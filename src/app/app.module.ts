bad8da20126f30f34d4239c5f4919e88fb2b7398
Nathan Víctor de Faria <nathan.faria@inatel.br>
2023-01-24 14:24:45 -0300

    create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,22 @@
+import { PackageListComponent } from './packages/package-list/package-list.component'
+import { PackagesComponent } from './packages/packages.component';
+import { HeaderComponent } from './header/header.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FormsModule } from "@angular/forms";
+import { FoodListComponent } from './food-list/food-list.component';
+import { FoodEditComponent } from './food-list/food-edit/food-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    PackagesComponent,
+    PackageListComponent,
+    FoodListComponent,
+    FoodEditComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/packages/package-list/package-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+    selector: 'app-package-list',
+    templateUrl: './package-list.component.html',
+    styleUrls: ['./package-list.component.css']
+})
+
+export class PackageListComponent{
+
+}
